1efc3d342bc20f22fa84d76e9308ca7bb56c2199
Vladislav <vladislav.bekreneff@yandex.ru>
2025-12-03 19:14:37 +0300

    Initial commit

---
+++ ticket-api-service/src/main/java/ru/vladislav/bekrenev/ticketapiservice/TicketApiServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vladislav.bekrenev.ticketapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketApiServiceApplication.class, args);
+    }
+
+}
---
+++ ticket-api-service/src/test/java/ru/vladislav/bekrenev/ticketapiservice/TicketApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladislav.bekrenev.ticketapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
